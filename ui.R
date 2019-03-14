99c9da862519fbe4f0856d4c891e3a3e7a141069
Ricardo Sanchez <rrs@rand.org>
2019-03-14 11:16:18 -0400

    cleaned up help text html
    also fixed some extra and unnecessary fluidRow() calls

--- ui.R
+++ ui.R
@@ -22,10 +22,8 @@ ui <- tagList(
       tabPanel(
         title = "Introduction", 
         value = "intro",
-        fluidRow(
-          # intro material was written by beth ann
-          includeHTML("html/introduction.html")
-        )
+        br(),
+        includeHTML("html/introduction.html")
       ),
       
       #
@@ -36,37 +34,35 @@ ui <- tagList(
         title = "File Upload",
         value = "upload",
         br(),
-        fluidRow(
-          sidebarPanel(
-            width = 3,
-            shinydashboard::box(
-              width = NULL,
-              
-              # select a file
-              fileInput("file1", "Choose CSV File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-              
-              # horizontal line
-              tags$hr(),
-              
-              # checkbox if file has header 
-              checkboxInput("header", "Header", TRUE),
-              
-              # select separator 
-              radioButtons("sep", "Separator", choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), selected = ","),
-              
-              # select quotes 
-              radioButtons("quote", "Quote", choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"), selected = '"')
-            )
-          ),
-          mainPanel(
-            width = 9,
-            shinydashboard::box(
-              id = "contents.box",
-              width = NULL,
-              
-              # show contents
-              DT::dataTableOutput("contents")
-            )
+        sidebarPanel(
+          width = 3,
+          shinydashboard::box(
+            width = NULL,
+            
+            # select a file
+            fileInput("file1", "Choose CSV File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+            
+            # horizontal line
+            tags$hr(),
+            
+            # checkbox if file has header 
+            checkboxInput("header", "Header", TRUE),
+            
+            # select separator 
+            radioButtons("sep", "Separator", choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), selected = ","),
+            
+            # select quotes 
+            radioButtons("quote", "Quote", choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"), selected = '"')
+          )
+        ),
+        mainPanel(
+          width = 9,
+          shinydashboard::box(
+            id = "contents.box",
+            width = NULL,
+            
+            # show contents
+            DT::dataTableOutput("contents")
           )
         )
       ),
@@ -78,49 +74,47 @@ ui <- tagList(
         title = "Propensity Score Model", 
         value = "model",
         br(),
-        fluidRow(
-          sidebarPanel(
-            width = 3,
-            shinydashboard::box(
-              width = NULL,
-
-              selectInput("treatment", "Treatment", ""),
-              
-              selectInput("outcome", "Outcome", "", multiple = TRUE),
-              
-              selectInput("covariates", "Covariates", "", multiple = TRUE),
-              
-              numericInput("n.trees", "gbm iterations", 5000),
-              
-              numericInput("interaction.depth", "Interaction depth", 2),
-              
-              textInput("shrinkage", "Shrinkage", "0.01"),
-              
-              selectInput("estimand", "Estimand", choices = c("ATE", "ATT"), selected = "ATE"),
-              
-              selectInput("stop.method", "Stop method", choices = stop.methods, selected = c("es.mean", "ks.max"), multiple = TRUE),
-              
-              selectInput("sampw", "Sampling weights", ""),
-              
-              numericInput("seed", "Seed", 42),
-              
-              actionButton("run", "Run", style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
-            )
-          ),
-          mainPanel(
-            width = 9,
-            shinydashboard::box(
-              id = "prop.score.box",
-              width = NULL,
-              
-              h3("Propensity Score Model"),
-              
-              # show the psm summary table
-              div(dataTableOutput("psm.summary"),style = "font-size: 85%; width: 100%")#,
-              
-              # save the summary table
-              #downloadButton("psm.summary.save", "save")
-            )
+        sidebarPanel(
+          width = 3,
+          shinydashboard::box(
+            width = NULL,
+            
+            selectInput("treatment", "Treatment", ""),
+            
+            selectInput("outcome", "Outcome", "", multiple = TRUE),
+            
+            selectInput("covariates", "Covariates", "", multiple = TRUE),
+            
+            numericInput("n.trees", "gbm iterations", 5000),
+            
+            numericInput("interaction.depth", "Interaction depth", 2),
+            
+            textInput("shrinkage", "Shrinkage", "0.01"),
+            
+            selectInput("estimand", "Estimand", choices = c("ATE", "ATT"), selected = "ATE"),
+            
+            selectInput("stop.method", "Stop method", choices = stop.methods, selected = c("es.mean", "ks.max"), multiple = TRUE),
+            
+            selectInput("sampw", "Sampling weights", ""),
+            
+            numericInput("seed", "Seed", 42),
+            
+            actionButton("run", "Run", style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+          )
+        ),
+        mainPanel(
+          width = 9,
+          shinydashboard::box(
+            id = "prop.score.box",
+            width = NULL,
+            
+            h3("Propensity Score Model"),
+            
+            # show the psm summary table
+            div(dataTableOutput("psm.summary"), style = "font-size: 85%; width: 100%")#,
+            
+            # save the summary table
+            #downloadButton("psm.summary.save", "save")
           )
         )
       ),
@@ -132,127 +126,125 @@ ui <- tagList(
         title = "Model Evaluation", 
         value = "eval",
         br(),
-        fluidRow(
-          navlistPanel(
-            widths = c(2,10),
-            tabPanel(
-              title = "Diagnostic Plots",
+        navlistPanel(
+          widths = c(2,10),
+          tabPanel(
+            title = "Diagnostic Plots",
+            
+            column(
+              # width
+              width = 8,
               
-              column(
-                # width
-                width = 8,
-                
-                # choose plot type (this is an argument passed to the twang plot function)
-                selectInput("diag.plot.select", "Plot", choices = plot.types[-3]),
-                
-                # choose stop method (this is an argument passed to the twang plot function)
-                selectInput("diag.plot.stopmethod", "Stop method", choices = stop.methods, selected = "", multiple = TRUE),
-                
-                # show the plot
-                plotOutput("diag.plot"),
-                
-                # save the plot
-                downloadButton("diag.plot.save", "save")
-              ),
+              # choose plot type (this is an argument passed to the twang plot function)
+              selectInput("diag.plot.select", "Plot", choices = plot.types[-3]),
               
-              column(
-                # width 
-                width = 4,
-                
-                # description of plots
-                wellPanel(
-                  includeHTML("html/diagnostic-plots.html")
-                )
-              )
+              # choose stop method (this is an argument passed to the twang plot function)
+              selectInput("diag.plot.stopmethod", "Stop method", choices = stop.methods, selected = "", multiple = TRUE),
+              
+              # show the plot
+              plotOutput("diag.plot"),
+              
+              # save the plot
+              downloadButton("diag.plot.save", "save")
             ),
-            tabPanel(
-              title = "Balance Plots",
+            
+            column(
+              # width 
+              width = 4,
               
-              column(
-                # width 
-                width = 8,
-                
-                # choose stop method (this is an argument passed to the twang plot function)
-                selectInput("bal.plot.stopmethod", "Stop method", choices = stop.methods, selected = "", multiple = TRUE),
-                
-                # show the plot
-                plotOutput("bal.plot"),
-                
-                # save the plot
-                downloadButton("bal.plot.save", "save")
-              ),
-              
-              column(
-                # width 
-                width = 4,
-                
-                # description of plots
-                wellPanel(
-                  includeHTML("html/balance-plots.html")
-                )
+              # description of plots
+              wellPanel(
+                includeHTML("html/diagnostic-plots.html")
               )
+            )
+          ),
+          tabPanel(
+            title = "Balance Plots",
+            
+            column(
+              # width 
+              width = 8,
+              
+              # choose stop method (this is an argument passed to the twang plot function)
+              selectInput("bal.plot.stopmethod", "Stop method", choices = stop.methods, selected = "", multiple = TRUE),
+              
+              # show the plot
+              plotOutput("bal.plot"),
+              
+              # save the plot
+              downloadButton("bal.plot.save", "save")
             ),
-            tabPanel(
-              title = "Balance Tables",
+            
+            column(
+              # width 
+              width = 4,
               
-              column(
-                # width 
-                width = 8,
-              
-                h3("Unweighted Balance Table"),
-                
-                # show the unweighted balance table
-                div(dataTableOutput("unweighted.balance.table"), style = "font-size: 85%; width: 100%"),
-                
-                # save the table
-                #downloadButton("unweighted.balance.table.save", "save"),
-                
-                h3("Weighted Balance Table"),
-                
-                # choose the stop method (this is an argument passed to the twang plot function)
-                selectInput("bal.stopmethod", "Stop method", ""),
-                
-                # show the weighted balance table
-                div(dataTableOutput("weighted.balance.table"), style = "font-size: 85%; width: 100%")
-                
-                # save the table 
-                #downloadButton("weighted.balance.table.save", "save")
-              ),
-              
-              column(
-                # width
-                width = 4,
-                
-                # description of tables
-                wellPanel(
-                  includeHTML("html/balance-tables.html")
-                )
+              # description of plots
+              wellPanel(
+                includeHTML("html/balance-plots.html")
               )
+            )
+          ),
+          tabPanel(
+            title = "Balance Tables",
+            
+            column(
+              # width 
+              width = 8,
+              
+              h3("Unweighted Balance Table"),
+              
+              # show the unweighted balance table
+              div(dataTableOutput("unweighted.balance.table"), style = "font-size: 85%; width: 100%"),
+              
+              # save the table
+              #downloadButton("unweighted.balance.table.save", "save"),
+              
+              h3("Weighted Balance Table"),
+              
+              # choose the stop method (this is an argument passed to the twang plot function)
+              selectInput("bal.stopmethod", "Stop method", ""),
+              
+              # show the weighted balance table
+              div(dataTableOutput("weighted.balance.table"), style = "font-size: 85%; width: 100%")
+              
+              # save the table 
+              #downloadButton("weighted.balance.table.save", "save")
             ),
-            tabPanel(
-              title = "Relative Influence",
+            
+            column(
+              # width
+              width = 4,
               
-              column(
-                # width
-                width = 8,
+              # description of tables
+              wellPanel(
+                includeHTML("html/balance-tables.html")
+              )
+            )
+          ),
+          tabPanel(
+            title = "Relative Influence",
+            
+            column(
+              # width
+              width = 8,
               
-                h3("Relative Influence"),
-                
-                # show the relative influence graph
-                plotOutput("rel.inf.plot"),
-                
-                # save the plot
-                downloadButton("rel.inf.plot.save", "save")
-              ),
+              h3("Relative Influence"),
               
-              column(
-                # width 
-                width = 4,
-                
-                # description of plots
-                wellPanel(
-                  includeHTML("html/relative_influence-plots.html")
-                )
+              # show the relative influence graph
+              plotOutput("rel.inf.plot"),
+              
+              # save the plot
+              downloadButton("rel.inf.plot.save", "save")
+            ),
+            
+            column(
+              # width 
+              width = 4,
+              
+              # description of plots
+              wellPanel(
+                includeHTML("html/relative_influence-plots.html")
               )
             )
           )
@@ -307,13 +299,10 @@ ui <- tagList(
       tabPanel(
         title = "Weights", 
         value = "weights",
-        fluidRow(
-          includeHTML("html/weights.html"),
-          
-          textInput("weight.var", "Enter name for new column", value = "w"),
-          
-          downloadButton("weights.save", "save")
-        )
+        br(),
+        includeHTML("html/weights.html"),
+        textInput("weight.var", "Enter name for new column", value = "w"),
+        downloadButton("weights.save", "save")
       )
       
     )
